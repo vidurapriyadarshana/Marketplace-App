89f9368f44bc99a797baa8248ea2805b5c225de9
Vidura Priyadarshana <vidura200427@gmail.com>
2026-01-13 16:25:27 +0530

    feat: add Firebase configuration and environment variables

---
+++ config/FirebaseConfig.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { ENV } from "./env";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: ENV.firebase.apiKey,
+  authDomain: ENV.firebase.authDomain,
+  projectId: ENV.firebase.projectId,
+  storageBucket: ENV.firebase.storageBucket,
+  messagingSenderId: ENV.firebase.messagingSenderId,
+  appId: ENV.firebase.appId,
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
---
+++ config/env.ts
@@ -0,0 +1,13 @@
+// Environment configuration
+// Access environment variables easily throughout the app
+
+export const ENV = {
+  firebase: {
+    apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY!,
+    authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+    projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID!,
+    storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+    messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+    appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID!,
+  },
+} as const;
